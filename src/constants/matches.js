c310899ba22b8b4c30dcfd0fb11931d7db1bcabd
cleame8922 <cleame8922@sungkyul.ac.kr>
2025-09-29 23:08:24 +0900

    [fix]예측 로직 추가 수정

--- src/constants/matches.js
+++ src/constants/matches.js
@@ -2,36 +2,36 @@ export const matches = [
   {
     sportType: "BASKETBALL",
     predictions: [
-      { predictionResult: "TEAM_A", teamName: "관광학과", image: "/assets/images/SchoolLogo.png" },
-      { predictionResult: "TEAM_B", teamName: "체육교육과", image: "/assets/images/SchoolLogo.png" },
+      { predictionResult: "TEAM_A", teamName: "관광학과", image: "/assets/images/Department/SchoolLogo.png" },
+      { predictionResult: "TEAM_B", teamName: "체육교육과", image: "/assets/images/Department/sport.png" },
     ],
   },
   {
     sportType: "DODGEBALL",
     predictions: [
-      { predictionResult: "TEAM_A", teamName: "기독교교육상담학과", image: "/assets/images/SchoolLogo.png" },
-      { predictionResult: "TEAM_B", teamName: "행정학부", image: "/assets/images/SchoolLogo.png" },
+      { predictionResult: "TEAM_A", teamName: "기독교교육상담학과", image: "/assets/images/Department/SchoolLogo.png" },
+      { predictionResult: "TEAM_B", teamName: "행정학부", image: "/assets/images/Department/policy.png" },
     ],
   },
   {
     sportType: "KICK_BASEBALL",
     predictions: [
-      { predictionResult: "TEAM_A", teamName: "신학과", image: "/assets/images/SchoolLogo.png" },
-      { predictionResult: "TEAM_B", teamName: "유아교육과", image: "/assets/images/SchoolLogo.png" },
+      { predictionResult: "TEAM_A", teamName: "신학과", image: "/assets/images/Department/theology.png" },
+      { predictionResult: "TEAM_B", teamName: "유아교육과", image: "/assets/images/Department/baby.png" },
     ],
   },
   {
     sportType: "FOOT_VOLLEY",
     predictions: [
-      { predictionResult: "TEAM_A", teamName: "체육교육과", image: "/assets/images/SchoolLogo.png" },
-      { predictionResult: "TEAM_B", teamName: "사회복지학과", image: "/assets/images/SchoolLogo.png" },
+      { predictionResult: "TEAM_A", teamName: "체육교육과", image: "/assets/images/Department/sport.png" },
+      { predictionResult: "TEAM_B", teamName: "사회복지학과", image: "/assets/images/Department/social.png" },
     ],
   },
   {
     sportType: "SOCCER",
     predictions: [
-      { predictionResult: "TEAM_A", teamName: "체육교육과", image: "/assets/images/SchoolLogo.png" },
-      { predictionResult: "TEAM_B", teamName: "행정학부", image: "/assets/images/SchoolLogo.png" },
+      { predictionResult: "TEAM_A", teamName: "체육교육과", image: "/assets/images/Department/sport.png" },
+      { predictionResult: "TEAM_B", teamName: "행정학부", image: "/assets/images/Department/policy.png" },
     ],
   },
 ];
